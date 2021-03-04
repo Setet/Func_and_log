7b4220682a607df5fcdf4fd4bbae439d01e1ce4b
Setet <72457086+Setet@users.noreply.github.com>
2021-03-04 22:24:26 +0300

    v1.00

---
+++ Laba2/Laba2.pl
@@ -0,0 +1,522 @@
+gender(it,0).
+gender(fahrenheit_451,0).
+gender(1984,0).
+gender(shantaram,0).
+gender(master_and_margarita,0).
+gender(three_companions,0).
+gender(atlas_will_straighten_his_shoulders,0).
+gender(catcher_in_the_rye,0).
+gender(flowers_for_algernon,0).
+gender(the_picture_of_dorian_grey,0).
+gender(little_prince,0).
+gender(dandelion_wine,0).
+gender(one_hundred_years_of_solitude,0).
+gender(to_kill_a_mockingbird,0).
+gender(bible,0).
+gender(crime_and_punishment,0).
+gender(anna_karenina,0).
+gender(twelve_chairs,0).
+gender(triumphal_arch,0).
+gender(life_on_loan,0).
+gender(count_of_monte_cristo,0).
+gender(idiot,0).
+gender(eugene_onegin,0).
+gender(harry_potter_and_the_philosophers_stone,0).
+gender(pride_and_prejudice,0).
+gender(the_great_gatsby,0).
+gender(fight_club,0).
+gender(book_thief,0).
+gender(green_mile,0).
+
+author(it,0).
+author(fahrenheit_451,0).
+author(1984,0).
+author(shantaram,0).
+author(master_and_margarita,0).
+author(three_companions,0).
+author(atlas_will_straighten_his_shoulders,0).
+author(catcher_in_the_rye,0).
+author(flowers_for_algernon,0).
+author(the_picture_of_dorian_grey,0).
+author(little_prince,0).
+author(dandelion_wine,0).
+author(one_hundred_years_of_solitude,0).
+author(to_kill_a_mockingbird,0).
+author(bible,0).
+author(crime_and_punishment,0).
+author(anna_karenina,0).
+author(twelve_chairs,0).
+author(triumphal_arch,0).
+author(life_on_loan,0).
+author(count_of_monte_cristo,0).
+author(idiot,0).
+author(eugene_onegin,0).
+author(harry_potter_and_the_philosophers_stone,0).
+author(pride_and_prejudice,0).
+author(the_great_gatsby,0).
+author(fight_club,0).
+author(book_thief,0).
+author(green_mile,0).
+
+year(it,0).
+year(fahrenheit_451,0).
+year(1984,0).
+year(shantaram,0).
+year(master_and_margarita,0).
+year(three_companions,0).
+year(atlas_will_straighten_his_shoulders,0).
+year(catcher_in_the_rye,0).
+year(flowers_for_algernon,0).
+year(the_picture_of_dorian_grey,0).
+year(little_prince,0).
+year(dandelion_wine,0).
+year(one_hundred_years_of_solitude,0).
+year(to_kill_a_mockingbird,0).
+year(bible,0).
+year(crime_and_punishment,0).
+year(anna_karenina,0).
+year(twelve_chairs,0).
+year(triumphal_arch,0).
+year(life_on_loan,0).
+year(count_of_monte_cristo,0).
+year(idiot,0).
+year(eugene_onegin,0).
+year(harry_potter_and_the_philosophers_stone,0).
+year(pride_and_prejudice,0).
+year(the_great_gatsby,0).
+year(fight_club,0).
+year(book_thief,0).
+year(green_mile,0).
+
+genre(it,0).
+genre(fahrenheit_451,0).
+genre(1984,0).
+genre(shantaram,0).
+genre(master_and_margarita,0).
+genre(three_companions,0).
+genre(atlas_will_straighten_his_shoulders,0).
+genre(catcher_in_the_rye,0).
+genre(flowers_for_algernon,0).
+genre(the_picture_of_dorian_grey,0).
+genre(little_prince,0).
+genre(dandelion_wine,0).
+genre(one_hundred_years_of_solitude,0).
+genre(to_kill_a_mockingbird,0).
+genre(bible,0).
+genre(crime_and_punishment,0).
+genre(anna_karenina,0).
+genre(twelve_chairs,0).
+genre(triumphal_arch,0).
+genre(life_on_loan,0).
+genre(count_of_monte_cristo,0).
+genre(idiot,0).
+genre(eugene_onegin,0).
+genre(harry_potter_and_the_philosophers_stone,0).
+genre(pride_and_prejudice,0).
+genre(the_great_gatsby,0).
+genre(fight_club,0).
+genre(book_thief,0).
+genre(green_mile,0).
+
+pages(it,0).
+pages(fahrenheit_451,0).
+pages(1984,0).
+pages(shantaram,0).
+pages(master_and_margarita,0).
+pages(three_companions,0).
+pages(atlas_will_straighten_his_shoulders,0).
+pages(catcher_in_the_rye,0).
+pages(flowers_for_algernon,0).
+pages(the_picture_of_dorian_grey,0).
+pages(little_prince,0).
+pages(dandelion_wine,0).
+pages(one_hundred_years_of_solitude,0).
+pages(to_kill_a_mockingbird,0).
+pages(bible,0).
+pages(crime_and_punishment,0).
+pages(anna_karenina,0).
+pages(twelve_chairs,0).
+pages(triumphal_arch,0).
+pages(life_on_loan,0).
+pages(count_of_monte_cristo,0).
+pages(idiot,0).
+pages(eugene_onegin,0).
+pages(harry_potter_and_the_philosophers_stone,0).
+pages(pride_and_prejudice,0).
+pages(the_great_gatsby,0).
+pages(fight_club,0).
+pages(book_thief,0).
+pages(green_mile,0).
+
+images(it,0).
+images(fahrenheit_451,0).
+images(1984,0).
+images(shantaram,0).
+images(master_and_margarita,0).
+images(three_companions,0).
+images(atlas_will_straighten_his_shoulders,0).
+images(catcher_in_the_rye,0).
+images(flowers_for_algernon,0).
+images(the_picture_of_dorian_grey,0).
+images(little_prince,0).
+images(dandelion_wine,0).
+images(one_hundred_years_of_solitude,0).
+images(to_kill_a_mockingbird,0).
+images(bible,0).
+images(crime_and_punishment,0).
+images(anna_karenina,0).
+images(twelve_chairs,0).
+images(triumphal_arch,0).
+images(life_on_loan,0).
+images(count_of_monte_cristo,0).
+images(idiot,0).
+images(eugene_onegin,0).
+images(harry_potter_and_the_philosophers_stone,0).
+images(pride_and_prejudice,0).
+images(the_great_gatsby,0).
+images(fight_club,0).
+images(book_thief,0).
+images(green_mile,0).
+
+character(it,0).
+character(fahrenheit_451,0).
+character(1984,0).
+character(shantaram,0).
+character(master_and_margarita,0).
+character(three_companions,0).
+character(atlas_will_straighten_his_shoulders,0).
+character(catcher_in_the_rye,0).
+character(flowers_for_algernon,0).
+character(the_picture_of_dorian_grey,0).
+character(little_prince,0).
+character(dandelion_wine,0).
+character(one_hundred_years_of_solitude,0).
+character(to_kill_a_mockingbird,0).
+character(bible,0).
+character(crime_and_punishment,0).
+character(anna_karenina,0).
+character(twelve_chairs,0).
+character(triumphal_arch,0).
+character(life_on_loan,0).
+character(count_of_monte_cristo,0).
+character(idiot,0).
+character(eugene_onegin,0).
+character(harry_potter_and_the_philosophers_stone,0).
+character(pride_and_prejudice,0).
+character(the_great_gatsby,0).
+character(fight_club,0).
+character(book_thief,0).
+character(green_mile,0).
+
+price(it,0).
+price(fahrenheit_451,0).
+price(1984,0).
+price(shantaram,0).
+price(master_and_margarita,0).
+price(three_companions,0).
+price(atlas_will_straighten_his_shoulders,0).
+price(catcher_in_the_rye,0).
+price(flowers_for_algernon,0).
+price(the_picture_of_dorian_grey,0).
+price(little_prince,0).
+price(dandelion_wine,0).
+price(one_hundred_years_of_solitude,0).
+price(to_kill_a_mockingbird,0).
+price(bible,0).
+price(crime_and_punishment,0).
+price(anna_karenina,0).
+price(twelve_chairs,0).
+price(triumphal_arch,0).
+price(life_on_loan,0).
+price(count_of_monte_cristo,0).
+price(idiot,0).
+price(eugene_onegin,0).
+price(harry_potter_and_the_philosophers_stone,0).
+price(pride_and_prejudice,0).
+price(the_great_gatsby,0).
+price(fight_club,0).
+price(book_thief,0).
+price(green_mile,0).
+
+appraisal(it,0).
+appraisal(fahrenheit_451,0).
+appraisal(1984,0).
+appraisal(shantaram,0).
+appraisal(master_and_margarita,0).
+appraisal(three_companions,0).
+appraisal(atlas_will_straighten_his_shoulders,0).
+appraisal(catcher_in_the_rye,0).
+appraisal(flowers_for_algernon,0).
+appraisal(the_picture_of_dorian_grey,0).
+appraisal(little_prince,0).
+appraisal(dandelion_wine,0).
+appraisal(one_hundred_years_of_solitude,0).
+appraisal(to_kill_a_mockingbird,0).
+appraisal(bible,0).
+appraisal(crime_and_punishment,0).
+appraisal(anna_karenina,0).
+appraisal(twelve_chairs,0).
+appraisal(triumphal_arch,0).
+appraisal(life_on_loan,0).
+appraisal(count_of_monte_cristo,0).
+appraisal(idiot,0).
+appraisal(eugene_onegin,0).
+appraisal(harry_potter_and_the_philosophers_stone,0).
+appraisal(pride_and_prejudice,0).
+appraisal(the_great_gatsby,0).
+appraisal(fight_club,0).
+appraisal(book_thief,0).
+appraisal(green_mile,0).
+
+happyending(it,0).
+happyending(fahrenheit_451,0).
+happyending(1984,0).
+happyending(shantaram,0).
+happyending(master_and_margarita,0).
+happyending(three_companions,0).
+happyending(atlas_will_straighten_his_shoulders,0).
+happyending(catcher_in_the_rye,0).
+happyending(flowers_for_algernon,0).
+happyending(the_picture_of_dorian_grey,0).
+happyending(little_prince,0).
+happyending(dandelion_wine,0).
+happyending(one_hundred_years_of_solitude,0).
+happyending(to_kill_a_mockingbird,0).
+happyending(bible,0).
+happyending(crime_and_punishment,0).
+happyending(anna_karenina,0).
+happyending(twelve_chairs,0).
+happyending(triumphal_arch,0).
+happyending(life_on_loan,0).
+happyending(count_of_monte_cristo,0).
+happyending(idiot,0).
+happyending(eugene_onegin,0).
+happyending(harry_potter_and_the_philosophers_stone,0).
+happyending(pride_and_prejudice,0).
+happyending(the_great_gatsby,0).
+happyending(fight_club,0).
+happyending(book_thief,0).
+happyending(green_mile,0).
+
+franchise(it,0).
+franchise(fahrenheit_451,0).
+franchise(1984,0).
+franchise(shantaram,0).
+franchise(master_and_margarita,0).
+franchise(three_companions,0).
+franchise(atlas_will_straighten_his_shoulders,0).
+franchise(catcher_in_the_rye,0).
+franchise(flowers_for_algernon,0).
+franchise(the_picture_of_dorian_grey,0).
+franchise(little_prince,0).
+franchise(dandelion_wine,0).
+franchise(one_hundred_years_of_solitude,0).
+franchise(to_kill_a_mockingbird,0).
+franchise(bible,0).
+franchise(crime_and_punishment,1).
+franchise(anna_karenina,1).
+franchise(twelve_chairs,1).
+franchise(triumphal_arch,1).
+franchise(life_on_loan,1).
+franchise(count_of_monte_cristo,1).
+franchise(idiot,1).
+franchise(eugene_onegin,1).
+franchise(harry_potter_and_the_philosophers_stone,1).
+franchise(pride_and_prejudice,1).
+franchise(the_great_gatsby,1).
+franchise(fight_club,1).
+franchise(book_thief,1).
+franchise(green_mile,1).
+
+classic(it,0).
+classic(fahrenheit_451,0).
+classic(1984,0).
+classic(shantaram,0).
+classic(master_and_margarita,0).
+classic(three_companions,0).
+classic(atlas_will_straighten_his_shoulders,0).
+classic(catcher_in_the_rye,0).
+classic(flowers_for_algernon,1).
+classic(the_picture_of_dorian_grey,1).
+classic(little_prince,1).
+classic(dandelion_wine,1).
+classic(one_hundred_years_of_solitude,1).
+classic(to_kill_a_mockingbird,1).
+classic(bible,1).
+classic(crime_and_punishment,0).
+classic(anna_karenina,0).
+classic(twelve_chairs,0).
+classic(triumphal_arch,0).
+classic(life_on_loan,0).
+classic(count_of_monte_cristo,1).
+classic(idiot,1).
+classic(eugene_onegin,1).
+classic(harry_potter_and_the_philosophers_stone,1).
+classic(pride_and_prejudice,1).
+classic(the_great_gatsby,0).
+classic(fight_club,0).
+classic(book_thief,0).
+classic(green_mile,1).
+
+nobel_prize(it,0).
+nobel_prize(fahrenheit_451,0).
+nobel_prize(1984,0).
+nobel_prize(shantaram,0).
+nobel_prize(master_and_margarita,1).
+nobel_prize(three_companions,1).
+nobel_prize(atlas_will_straighten_his_shoulders,1).
+nobel_prize(catcher_in_the_rye,1).
+nobel_prize(flowers_for_algernon,0).
+nobel_prize(the_picture_of_dorian_grey,0).
+nobel_prize(little_prince,0).
+nobel_prize(dandelion_wine,1).
+nobel_prize(one_hundred_years_of_solitude,1).
+nobel_prize(to_kill_a_mockingbird,1).
+nobel_prize(bible,1).
+nobel_prize(crime_and_punishment,0).
+nobel_prize(anna_karenina,0).
+nobel_prize(twelve_chairs,0).
+nobel_prize(triumphal_arch,1).
+nobel_prize(life_on_loan,1).
+nobel_prize(count_of_monte_cristo,0).
+nobel_prize(idiot,1).
+nobel_prize(eugene_onegin,1).
+nobel_prize(harry_potter_and_the_philosophers_stone,1).
+nobel_prize(pride_and_prejudice,0).
+nobel_prize(the_great_gatsby,1).
+nobel_prize(fight_club,0).
+nobel_prize(book_thief,1).
+nobel_prize(green_mile,0).
+
+language(it,0).
+language(fahrenheit_451,0).
+language(1984,1).
+language(shantaram,1).
+language(master_and_margarita,0).
+language(three_companions,0).
+language(atlas_will_straighten_his_shoulders,1).
+language(catcher_in_the_rye,1).
+language(flowers_for_algernon,0).
+language(the_picture_of_dorian_grey,0).
+language(little_prince,1).
+language(dandelion_wine,0).
+language(one_hundred_years_of_solitude,1).
+language(to_kill_a_mockingbird,0).
+language(bible,1).
+language(crime_and_punishment,0).
+language(anna_karenina,0).
+language(twelve_chairs,1).
+language(triumphal_arch,0).
+language(life_on_loan,1).
+language(count_of_monte_cristo,1).
+language(idiot,0).
+language(eugene_onegin,1).
+language(harry_potter_and_the_philosophers_stone,0).
+language(pride_and_prejudice,1).
+language(the_great_gatsby,1).
+language(fight_club,1).
+language(book_thief,0).
+language(green_mile,0).
+
+film(it,0).
+film(fahrenheit_451,1).
+film(1984,0).
+film(shantaram,1).
+film(master_and_margarita,0).
+film(three_companions,1).
+film(atlas_will_straighten_his_shoulders,0).
+film(catcher_in_the_rye,1).
+film(flowers_for_algernon,0).
+film(the_picture_of_dorian_grey,1).
+film(little_prince,1).
+film(dandelion_wine,1).
+film(one_hundred_years_of_solitude,0).
+film(to_kill_a_mockingbird,0).
+film(bible,1).
+film(crime_and_punishment,0).
+film(anna_karenina,1).
+film(twelve_chairs,1).
+film(triumphal_arch,1).
+film(life_on_loan,0).
+film(count_of_monte_cristo,0).
+film(idiot,0).
+film(eugene_onegin,0).
+film(harry_potter_and_the_philosophers_stone,1).
+film(pride_and_prejudice,1).
+film(the_great_gatsby,1).
+film(fight_club,0).
+film(book_thief,0).
+film(green_mile,0).
+
+question1(X1):-	write("1)The author is a woman?"),nl,
+				write("1.yes"),nl,
+				write("0.no"),nl,
+				read(X1).
+question2(X2):-	write("2)An author from Russia?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X2).
+question3(X3):-	write("3)Year of issue 2012?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X3).
+question4(X4):-	write("4)Genre fiction?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X4).
+question5(X5):-	write("5)More than 500 pages?"),nl,
+                                write("1.yes"),nl,
+				write("0.no"),nl,
+                                read(X5).
+question6(X6):-	write("6)Got pictures?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X6).
+question7(X7):-	write("7)The main character is a man?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X7).
+question8(X8):-	write("8)Is the book worth more than $ 10?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X8).
+question9(X9):-	write("9)Are the ratings on RoyalLib more than 6.00?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X9).
+question10(X10):-write("10)Is there a happy ending at the end of the book?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X10).
+question11(X11):-write("11)Is this book part of a franchise?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X11).
+question12(X12):-write("12)Is this book considered a classic?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X12).
+question13(X13):-write("13)Did the author receive a Nobel Prize for this book? "),nl,
+                                write("1.yes"),nl,
+				write("0.no"),nl,
+                                read(X13).
+question14(X14):-write("14)Is the book written in Russian?"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X14).
+question15(X15):-write("15)Did they make a movie based on the book??"),nl,
+				write("1. yes"),nl,
+				write("0. no"),nl,
+				read(X15).
+
+pr:-	question1(X1),question2(X2),question3(X3),question4(X4),
+		question5(X5),question6(X6),question7(X7),question8(X8),
+                question9(X9),question10(X10),question11(X11),question12(X12),
+                question13(X13),question14(X14),question15(X15),
+
+		gender(X,X1),author(X,X2),year(X,X3),genre(X,X4),
+                pages(X,X5),images(X,X6),character(X,X7),price(X,X8),
+                appraisal(X,X9),happyending(X,X10),franchise(X,X11),classic(X,X12),
+                nobel_prize(X,X13),language(X,X14),film(X,X15),
+		write(X).
+
