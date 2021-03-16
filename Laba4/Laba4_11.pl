be50522a90180b491a7acebbc38084ce8479bfab
Setet <72457086+Setet@users.noreply.github.com>
2021-03-16 21:57:02 +0300

    v1.02

--- Laba4/Laba4_11.pl
+++ Laba4/Laba4_11.pl
@@ -1,2 +1,5 @@
-p([],[]):-!.
-p([HS|Sublist],[HL|List]):-p(Sublist,List),HS =:= HL.
+ravenstvo([H],[H|_]):-!.
+ravenstvo([H|_],[H]):-!.
+ravenstvo([H|T],[H1|T1]):- H is H1 ,ravenstvo(T,T1).
+sravn([H|T],[H1|T1]):-ravenstvo([H|T],[H1|T1]).
+sravn([H|T],[_|T1]):-sravn([H|T],T1).
