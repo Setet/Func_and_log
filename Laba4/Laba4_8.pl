3699aedc13d21383aea5c599099c979417c7591b
Setet <72457086+Setet@users.noreply.github.com>
2021-03-16 21:29:11 +0300

    v1.01

--- Laba4/Laba4_8.pl
+++ Laba4/Laba4_8.pl
@@ -5,10 +5,11 @@ rList(N,I,CurL,L):-I1 is I+1, read(X),append(CurL,[X],CurL1),rList(N,I1,CurL1,L)
 min(X,Y,X):-X<Y,!.
 min(_,Y,Y):-!.
 
-min_list_up([],9999):-!.
-min_list_up([H|List],Min):-min_list_up(List,NewMin),min(NewMin,H,Min).
+min_list_down([H|T],Min):-min_l_d([H|T],H,Min).
+min_l_d([],Min,Min):-!.
+min_l_d([H|T],Tec,Min):-min(Tec,H,Tec1),min_l_d(T,Tec1,Min).
 
 read_min_elem_list(N):-
     write("List: "),readList(N,List),nl,
-    write("Min elem =: "),min_list_up(List,X),
+    write("Min elem = "),min_list_down(List,X),
     write(X),nl,false.
