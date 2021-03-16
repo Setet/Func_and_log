11b651dd285a7675756123e3c6b439a4c3bfbb37
Setet <72457086+Setet@users.noreply.github.com>
2021-03-16 17:11:31 +0300

    v1.01

--- Laba3/Laba3_9.pl
+++ Laba3/Laba3_9.pl
@@ -1,4 +1,3 @@
-min_1(X,Min):-X < 10, Min is X,!.
-min_1(X,Min):-X>=10,X1 is X mod 10, X2 is X div 10, min_1(X2,Min),Min > X1,Min is X1.
-min_1(X,Min):-X>=10,X1 is X mod 10, X2 is X div 10, min_1(X2,Min),Min < X1,Min is Min.
-min:-read(X), min_1(X,Min), writeln(Min).
+min(X,M):-M1 is X mod 10,X1 is X div 10,min(X1,M,M1).
+min(X,M,Mt):- X1 is X div 10, X1 is 0,M is Mt,!.
+min(X,M,Mt):-((Mt>X mod 10,M1 is X mod 10,!);(M1 is Mt,!)),X1 is X div 10,!,min(X1,M,M1).
