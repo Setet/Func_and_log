000b30719ce7f86c497a9e86e49101885c6c6305
Setet <72457086+Setet@users.noreply.github.com>
2021-03-16 21:00:25 +0300

    v1.01

--- Laba4/Laba4_7.pl
+++ Laba4/Laba4_7.pl
@@ -1,7 +1,6 @@
 min(X,Y,X):-X<Y,!.
 min(_,Y,Y):-!.
 
-minLD([],Min,Min):-!.
-minLD([H|L],CurMin,Min):-min(CurMin,H,NewCurMin),minLD(L,NewCurMin,Min).
-
-min_list_down(List,Min):-minLD(List,9999,Min).
+min_list_down([H|T],Min):-min_l_d([H|T],H,Min).
+min_l_d([],Min,Min):-!.
+min_l_d([H|T],Tec,Min):-min(Tec,H,Tec1),min_l_d(T,Tec1,Min).
