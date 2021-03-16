ee6afc11d64d3ff418ea1a65a0fdf32fde337a37
Setet <72457086+Setet@users.noreply.github.com>
2021-03-16 20:36:18 +0300

    v1.01

--- Laba4/Laba4_6.pl
+++ Laba4/Laba4_6.pl
@@ -1,5 +1,5 @@
 min(X,Y,X):-X<Y,!.
 min(_,Y,Y):-!.
 
-min_list_up([],9999):-!.
-min_list_up([H|List],Min):-min_list_up(List,NewMin),min(NewMin,H,Min).
+min_list_up([H],H):-!.
+min_list_up([H|T],Min):-min_list_up(T,Min1),min(H,Min1,Min).
