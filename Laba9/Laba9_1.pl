6b0209e4ec214c19de22767cfdc7bc1d6b017e60
Setet <72457086+Setet@users.noreply.github.com>
2021-03-31 15:46:59 +0300

    v1.00

---
+++ Laba9/Laba9_1.pl
@@ -0,0 +1,50 @@
+in_list([El|_],El).
+in_list([_|T],El):-in_list(T,El).
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+read_str(A,N,Flag):-get0(X),r_str(X,A,[],N,0,Flag).
+r_str(-1,A,A,N,N,1):-!.
+r_str(10,A,A,N,N,0):-!.
+r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Flag).
+
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+
+b_a_rp(_,0,Perm1):-write_str(Perm1),nl,!,fail.
+b_a_rp(A,N,Perm):-in_list(A,El),N1 is N-1,b_a_rp(A,N1,[El|Perm]).
+
+b_a_p([],Perm1):-write_str(Perm1),nl,!,fail.
+b_a_p(A,Perm):-in_list_exlude(A,El,A1),b_a_p(A1,[El|Perm]).
+
+in_list_exlude([El|T],El,T).
+in_list_exlude([H|T],El,[H|Tail]):-in_list_exlude(T,El,Tail).
+
+b_a_r(_,0,Perm1):-write_str(Perm1),nl,!,fail.
+b_a_r(A,N,Perm):-in_list_exlude(A,El,Perm1),N1 is N-1,b_a_r(Perm1,N1,[El|Perm]).
+
+sub_set([],[]).
+sub_set([H|Sub_set],[H|Set]):-sub_set(Sub_set,Set).
+sub_set(Sub_set,[_|Set]):-sub_set(Sub_set,Set).
+
+sochet([],0,_):-!.
+sochet([H|Sub_set],K,[H|Set]):-K1 is K-1,sochet(Sub_set,K1,Set).
+sochet(Sub_set,K,[_|Set]):-sochet(Sub_set,K,Set).
+
+sochet_p([],0,_):-!.
+sochet_p([H|Sub_set],K,[H|Set]):-K1 is K-1,sochet_p(Sub_set,K1,[H|Set]).
+sochet_p(Sub_set,K,[_|Set]):-sochet_p(Sub_set,K,Set).
+
+build_all_razm_p:-read_str(A,_),read(K),b_a_rp(A,K,[]).
+
+build_all_perm:-read_str(A,_),b_a_p(A,[]).
+
+build_all_razm:-read_str(A,_,_),read(K),b_a_r(A,K,[]).
+
+pr_subset:-read_str(A,_),sub_set(B,A),write_str(B),nl,fail.
+
+pr_sochet:-read_str(A,_),read(K),sochet(B,K,A),write_str(B),nl,fail.
+
+pr_sochet_p:-read_str(A,_),read(K),sochet_p(B,K,A),write_str(B),nl,fail.
